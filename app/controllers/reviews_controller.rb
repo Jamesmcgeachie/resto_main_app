ffc4856478aff5ef29cc8623f4ea93e5a317b915
James McGeachie <jamesmcgeachie10@gmail.com>
2015-10-19 19:10:28 -0400

    Removed my review pages and added in on show restaurant page, as per Fabio's lesson

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,8 +1,27 @@
 class ReviewsController < ApplicationController
 
+before_action :get_restaurant
+
+def new
+end
+
+def create
+    @review = @restaurant.reviews.build(review_params)
+    @review.save
+end
+
 def index
-	@restaurant = Restaurant.find(params[:restaurant_id])
-	@reviews = @restaurant.reviews
+end
+
+
+private
+
+def review_params
+	params.require(:review).permit(:content)
+end
+
+def get_restaurant
+    @restaurant = Restaurant.find(params[:restaurant_id])
 end
 
 end	
