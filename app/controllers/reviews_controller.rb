ad14179fe7469db3718712c9cd6b450cbee86e6a
James McGeachie <jamesmcgeachie10@gmail.com>
2015-10-19 17:14:39 -0400

    Added reviews index view and started off adding in the AJAX method for new review

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,3 +1,8 @@
 class ReviewsController < ApplicationController
 
-	
+def index
+	@restaurant = Restaurant.find(params[:restaurant_id])
+	@reviews = @restaurant.reviews
+end
+
+end	
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -2,6 +2,7 @@ class Restaurant < ActiveRecord::Base
 
 	belongs_to :cuisine_type
 	has_many :reservations
+	has_many :reviews
 
 	validates :name, :capacity, presence: true
 
