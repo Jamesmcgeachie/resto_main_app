71d7c4e3041990983b016348334fca8c64be3891
James McGeachie <jamesmcgeachie10@gmail.com>
2015-10-26 20:15:09 -0400

    installed postgres gem and changed the database.yml file with the postgres information

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20151019021924) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "cuisine_types", force: :cascade do |t|
     t.string   "name"
     t.datetime "created_at", null: false
